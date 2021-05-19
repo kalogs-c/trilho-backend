f9b49813bea9cfbce02a23e76b21fae32b30d250
Carlos Henrique Camilo <carloscamilocontato@gmail.com>
2021-05-19 16:05:44 -0300

    done

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,18 @@
 const formDiv = document.querySelector('.form')
 const container = document.querySelector('.container')
 const button = document.querySelector('.open-add_button')
+const closeButton = document.querySelector('.close-button')
 
-button.addEventListener('click',() => {
+
+const showAddPage = () => {
     container.classList.add('blur')
     formDiv.style.display = 'block'
-})
+}
+
+const hideAddPage = () => {
+    container.classList.remove('blur')
+    formDiv.style.display = 'none'
+}
+
+button.addEventListener('click', showAddPage)
+closeButton.addEventListener('click', hideAddPage)
